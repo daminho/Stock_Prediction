0c2574a60c01096dc4814b5d744d66e3401a8c1b
daminho <dangminhhoangdz@gmail.com>
2021-02-06 20:00:51 +0700

    just do it

--- Crawler.py
+++ Crawler.py
@@ -6,17 +6,24 @@ from bs4 import BeautifulSoup
 
 def str2num(x):
     res,cnt = 0,0
-    for i in x:
-        if(cnt==0 and not (i==',' or i=='.')):
-            res = res*10+ord(i)-ord('0')
+    s = 0
+    if(x[0]=='-'):
+        s+=1
+    for i in range(s,len(x)):
+        if(cnt==0 and not (x[i]==',' or x[i]=='.')):
+            res = res*10+ord(x[i])-ord('0')
             continue
-        elif(i=='.'):
+        elif(x[i]=='.'):
             cnt=1
             continue
-        elif(i!=','):
-            res = res+(ord(i)-ord('0'))*(1/(10**cnt))
+        elif(x[i]!=','):
+            res = res+(ord(x[i])-ord('0'))*(1/(10**cnt))
             cnt+=1
-    return round(res,2)
+    if(s==1):
+        s=-1
+    else:
+        s=1
+    return s*round(res,2)
 
 def process_text(x):
     res = ""
--- Process_Data.py
+++ Process_Data.py
@@ -1,5 +1,32 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
+import cv2
 prices = pd.read_csv("MBB_stock_price.csv")
 
-print(prices)
+def str2num(x):
+    res,cnt = 0,0
+    for i in x:
+        if(cnt==0 and not (i==',' or i=='.')):
+            res = res*10+ord(i)-ord('0')
+            continue
+        elif(i=='.'):
+            cnt=1
+            continue
+        elif(i!=','):
+            res = res+(ord(i)-ord('0'))*(1/(10**cnt))
+            cnt+=1
+    return round(res,2)
+
+average = []
+length = len(prices.index)
+for i in range(1,length):
+    row = list(prices.iloc[i].values)
+    average.append(round((row[5]+row[7])/2,2))
+class Price:
+    open, close, highest, lowest = float, float, float, float
+    volume = int
+
+class Stock:
+    date,name = str,str
+
+
